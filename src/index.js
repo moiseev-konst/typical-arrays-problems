608f1eb9a24129824bc9689e92091bee35319fec
Konstantsin Moiseev <moiseev.konst@gmail.com>
2021-08-19 17:18:51 +0300

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+exports.min = function min(array) {
+    if (!array || !array.length) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    return array.sort((a, b) => { return a - b })[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || !array.length) return 0;
+    return array.sort((a, b) => { return b - a })[0]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || !array.length) return 0;
+    return array.reduce((acc, curr) => { return acc + curr }) / array.length
 }
